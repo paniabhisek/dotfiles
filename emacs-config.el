8cd25a6240811d50c5086f7dad310eba694af0e9
Abhisek Panigrahi <pnabhisek@gmail.com>
2024-04-20 16:40:52 +0530

    Windows: Add initial load-env-vars package

--- emacs-config.el
+++ emacs-config.el
@@ -1,50 +1,3 @@
-(load-theme 'leuven t)
-
-(set-frame-font "Iosevka 13" nil t)
-
-(use-package all-the-icons
-  :if (display-graphic-p)
-  :config
-  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
-
-(fset 'getlink
-      (lambda (&optional arg) 
-	"Keyboard macro." 
-	(interactive "p") 
-	(kmacro-exec-ring-item (quote ("\C-c\C-l\C-a\C-k\C-g" 0 "%d")) arg)))
-(define-key org-mode-map (kbd "C-c l") #'getlink)
-
-(with-eval-after-load 'org
-  (add-hook 'org-mode-hook #'visual-line-mode))
-
-(with-eval-after-load 'org
-  (add-hook 'org-mode-hook #'org-appear-mode)
-  (setq org-appear-autolinks t))
-
-(global-set-key (kbd "C-c a") 'org-agenda)
-
-(global-set-key (kbd "C-c j") 'org-journal-new-entry)
-(setq org-journal-dir "d:/notes/journal")
-(setq org-journal-date-format "%A, %d %B %Y")
-(setq org-journal-file-format "%F")
-(require 'org-journal)
-
-(set-register ?H '(file . "d:/schedules/home-life.org"))
-
-(use-package org-modern
-  :hook
-  (org-mode . org-modern-mode)
-  (org-agenda-finalize . org-modern-agenda))
-
-(require 'eval-in-repl)
-(require 'python) ; if not done elsewhere
-(require 'eval-in-repl-python)
-(add-hook 'python-mode-hook
-          '(lambda ()
-	  (local-set-key (kbd "<C-return>") 'eir-eval-in-python)))
-
-(setq-default py-shell-name "d:/envs/emacs/Scripts/python.exe")
-
 (use-package yasnippet
   :ensure t
   :config
@@ -87,3 +40,53 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (setq blink-cursor-mode nil)
+
+(use-package load-env-vars)
+(load-env-vars "~/.emacs.d/.emacs.windows.env")
+
+(load-theme 'leuven t)
+
+(set-frame-font "Iosevka 13" nil t)
+
+(use-package all-the-icons
+  :if (display-graphic-p)
+  :config
+  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
+
+(fset 'getlink
+      (lambda (&optional arg) 
+	"Keyboard macro." 
+	(interactive "p") 
+	(kmacro-exec-ring-item (quote ("\C-c\C-l\C-a\C-k\C-g" 0 "%d")) arg)))
+(define-key org-mode-map (kbd "C-c l") #'getlink)
+
+(with-eval-after-load 'org
+  (add-hook 'org-mode-hook #'visual-line-mode))
+
+(with-eval-after-load 'org
+  (add-hook 'org-mode-hook #'org-appear-mode)
+  (setq org-appear-autolinks t))
+
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+(global-set-key (kbd "C-c j") 'org-journal-new-entry)
+(setq org-journal-dir (getenv "ORG_JOURNAL_DIR"))
+(setq org-journal-date-format "%A, %d %B %Y")
+(setq org-journal-file-format "%F")
+(require 'org-journal)
+
+(set-register ?H (cons 'file (getenv "HOME_LIFE")))
+
+(use-package org-modern
+  :hook
+  (org-mode . org-modern-mode)
+  (org-agenda-finalize . org-modern-agenda))
+
+(require 'eval-in-repl)
+(require 'python) ; if not done elsewhere
+(require 'eval-in-repl-python)
+(add-hook 'python-mode-hook
+          '(lambda ()
+	  (local-set-key (kbd "<C-return>") 'eir-eval-in-python)))
+
+(setq-default py-shell-name "d:/envs/emacs/Scripts/python.exe")
